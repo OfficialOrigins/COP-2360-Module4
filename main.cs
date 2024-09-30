41fbef752bf474094fb4a3209e05eaf38b954bd2
OfficialOrigins <138532627+OfficialOrigins@users.noreply.github.com>
2024-09-29 20:19:12 -0400

    Update main.cs

--- main.cs
+++ main.cs
@@ -29,4 +29,70 @@ class Program
       Console.WriteLine("This number is not a perfect number!");
     }
 }
-  
+
+//HomeSales Module 4: Exercise 4.2 
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int totalSalesD = 0;
+        int totalSalesE = 0;
+        int totalSalesF = 0;
+        int saleAmount = 0;
+        string salesChoice;
+
+        while (true)
+        {
+            Console.Write("Enter salesperson initial (D, E, F) or 'Z' to stop: ");
+            salesChoice = Console.ReadLine().ToUpper();
+
+            if (salesChoice == "Z")
+                break;
+
+            if (salesChoice != "D" && salesChoice != "E" && salesChoice != "F")
+            {
+                Console.WriteLine("Error, invalid salesperson selected. Please try again.");
+            }
+            else
+            {
+                Console.Write("Enter the sale amount: ");
+                saleAmount = Convert.ToInt32(Console.ReadLine());
+
+                if (salesChoice == "D")
+                {
+                    totalSalesD += saleAmount;
+                }
+                else if (salesChoice == "E")
+                {
+                    totalSalesE += saleAmount;
+                }
+                else if (salesChoice == "F")
+                {
+                    totalSalesF += saleAmount;
+                }
+            }
+        }
+
+        int grandTotal = totalSalesD + totalSalesE + totalSalesF;
+
+        Console.WriteLine("Salesperson D total: " + totalSalesD);
+        Console.WriteLine("Salesperson E total: " + totalSalesE);
+        Console.WriteLine("Salesperson F total: " + totalSalesF);
+        Console.WriteLine("Grand Total: " + grandTotal);
+
+        if (totalSalesD > totalSalesE && totalSalesD > totalSalesF)
+        {
+            Console.WriteLine("Highest Sale: D");
+        }
+        else if (totalSalesE > totalSalesD && totalSalesE > totalSalesF)
+        {
+            Console.WriteLine("Highest Sale: E");
+        }
+        else if (totalSalesF > totalSalesD && totalSalesF > totalSalesE)
+        {
+            Console.WriteLine("Highest Sale: F");
+        }
+    }
+}
