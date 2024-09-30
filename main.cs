fa50ecbcc628eef4ff6ba1bef8c85e137411cf7e
OfficialOrigins <138532627+OfficialOrigins@users.noreply.github.com>
2024-09-29 20:19:31 -0400

    Update main.cs comments

--- main.cs
+++ main.cs
@@ -1,34 +1,34 @@
 //Perfect Module 4 Exercise 4.1 
-using System;
+// using System;
 
-class Program 
-{
-  public static void Main (string[] args) 
-  {
-    //vars 
-    int number; 
-    int sum = 0; 
+// class Program 
+// {
+//   public static void Main (string[] args) 
+//   {
+//     //vars 
+//     int number; 
+//     int sum = 0; 
 
-    //code 
-    Console.Write("Please enter a number between 1 - 10000: ");
-    number = int.Parse(Console.ReadLine()); 
+//     //code 
+//     Console.Write("Please enter a number between 1 - 10000: ");
+//     number = int.Parse(Console.ReadLine()); 
 
-    for(int i = 1; i < number; ++i)
-    {
-      if(number % i == 0){
-        sum += i; 
-      }
-    }
+//     for(int i = 1; i < number; ++i)
+//     {
+//       if(number % i == 0){
+//         sum += i; 
+//       }
+//     }
 
-    if(number == sum)
-    {
-      Console.WriteLine("This number is a perfect number!");
-    }
+//     if(number == sum)
+//     {
+//       Console.WriteLine("This number is a perfect number!");
+//     }
 
-    if(number != sum)
-      Console.WriteLine("This number is not a perfect number!");
-    }
-}
+//     if(number != sum)
+//       Console.WriteLine("This number is not a perfect number!");
+//     }
+// }
 
 //HomeSales Module 4: Exercise 4.2 
 using System;
